5dc9b0849a4e151b2ff7f4d2612cd0175e1c7789
pmnguyenn <24521205@gm.uit.edu.vn>
2025-12-02 08:59:03 +0700

    Refactor Tetris to use graphics and add game over
    Updated the Tetris game to use graphics for rendering and added game over functionality.

--- tetris.cpp
+++ tetris.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <ctime>
+#include <graphics.h>
 using namespace std;
+
+#define CELL_SIZE 30
 #define H 20
 #define W 15
+
+const int SCREEN_WIDTH  = W * CELL_SIZE; 
+const int SCREEN_HEIGHT = H * CELL_SIZE;
+
+#define COLOR_EMPTY  BLACK
+#define COLOR_BORDER DARKGRAY
+
 char board[H][W] = {};
 char blocks[][4][4] = {
         {{' ','I',' ',' '},
@@ -73,10 +84,12 @@ char blocks[][4][4] = {
 };
 
 int x = 4, y = 0, b = 1;
+
 void gotoxy(int x, int y) {
     COORD c = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
+
 void boardDelBlock() {
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
@@ -95,12 +108,38 @@ void initBoard() {
             if ((i == H - 1) || (j == 0) || (j == W - 1)) board[i][j] = '#';
             else board[i][j] = ' ';
 }
+
 void draw() {
-    gotoxy(0, 0);
-    for (int i = 0; i < H; i++, cout << endl)
-        for (int j = 0; j < W; j++)
-            cout << board[i][j];
+    cleardevice(); 
+
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            if (board[i][j] == ' ') continue;
+
+            int x_pixel = j * CELL_SIZE;
+            int y_pixel = i * CELL_SIZE;
+
+            int color = WHITE; 
+            switch (board[i][j]) {
+                case '#': color = LIGHTGRAY; break;
+                case 'I': color = CYAN; break;
+                case 'O': color = YELLOW; break;
+                case 'T': color = MAGENTA; break;
+                case 'S': color = GREEN; break;
+                case 'Z': color = RED; break;
+                case 'J': color = BLUE; break;
+                case 'L': color = BROWN; break;
+            }
+
+            setfillstyle(SOLID_FILL, color);
+            bar(x_pixel, y_pixel, x_pixel + CELL_SIZE, y_pixel + CELL_SIZE); 
+
+            setcolor(BLACK);
+            rectangle(x_pixel, y_pixel, x_pixel + CELL_SIZE, y_pixel + CELL_SIZE);
+        }
+    }
 }
+
 bool canMove(int dx, int dy) {
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
@@ -112,6 +151,7 @@ bool canMove(int dx, int dy) {
             }
     return true;
 }
+
 void removeLine() {
     int j;
     for (int i = H - 2; i > 0; i--) {
@@ -122,7 +162,7 @@ void removeLine() {
                 for (int j = 0; j < W - 1; j++) board[ii][j] = board[ii - 1][j];
             i++;
             draw();
-            _sleep(200);
+            delay(200);
         }
     }
 }
@@ -131,7 +171,9 @@ int main()
 {
     srand(time(0));
     b = rand() % 7;
-    system("cls");
+    
+    initwindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tetris Game - SV3 Graphics");
+    
     initBoard();
     while (1) {
         boardDelBlock();
@@ -142,15 +184,27 @@ int main()
             if (c == 'x' && canMove(0, 1))  y++;
             if (c == 'q') break;
         }
-        if (canMove(0, 1)) y++;
-        else {
+        
+        if (canMove(0, 1)) {
+            y++;
+        } else {
             block2Board();
             removeLine();
             x = 5; y = 0; b = rand() % 7;
+            
+            if (!canMove(0, 0)) {
+                settextstyle(DEFAULT_FONT, HORIZ_DIR, 3);
+                outtextxy(W * CELL_SIZE / 2 - 100, H * CELL_SIZE / 2 - 20, "GAME OVER");
+                getch();
+                break;
+            }
         }
+        
         block2Board();
         draw();
-        _sleep(200);
+        delay(200);
     }
+    
+    closegraph();
     return 0;
 }
