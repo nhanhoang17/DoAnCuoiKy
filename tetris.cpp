ae083d7489356e44ce4c3ab7299aecbfbdac8d42
pmnguyenn <24521205@gm.uit.edu.vn>
2025-12-21 14:50:18 +0700

    fix đa hình xoay
    Added rotateBack method to Piece class and updated rotation logic in main game loop.

--- tetris.cpp
+++ tetris.cpp
@@ -7,18 +7,25 @@ using namespace std;
 
 #define H 20
 #define W 15
-int speed = 200;
-char board[H][W] = {};
 
+int speed = 200;
+char board[H][W];
 
 class Piece {
 protected:
     char shape[4][4];
 public:
     virtual void rotate() = 0;
-    char get(int i, int j) const {
-        return shape[i][j];
+    char get(int i, int j) const { return shape[i][j]; }
+
+    void rotateBack() {
+        char tmp[4][4];
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                tmp[3 - j][i] = shape[i][j];
+        memcpy(shape, tmp, sizeof(shape));
     }
+
     virtual ~Piece() {}
 };
 
@@ -53,8 +60,7 @@ public:
         };
         memcpy(shape, tmp, sizeof(shape));
     }
-    void rotate() override { 
-    }
+    void rotate() override {}
 };
 
 class TPiece : public Piece {
@@ -77,89 +83,8 @@ public:
     }
 };
 
-class LPiece : public Piece {
-public:
-    LPiece() {
-        char tmp[4][4] = {
-            {' ',' ','L',' '},
-            {'L','L','L',' '},
-            {' ',' ',' ',' '},
-            {' ',' ',' ',' '}
-        };
-        memcpy(shape, tmp, sizeof(shape));
-    }
-    void rotate() override {
-        char tmp[4][4];
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                tmp[j][3 - i] = shape[i][j];
-        memcpy(shape, tmp, sizeof(shape));
-    }
-};
-
-class JPiece : public Piece {
-public:
-    JPiece() {
-        char tmp[4][4] = {
-            {'J',' ',' ',' '},
-            {'J','J','J',' '},
-            {' ',' ',' ',' '},
-            {' ',' ',' ',' '}
-        };
-        memcpy(shape, tmp, sizeof(shape));
-    }
-    void rotate() override {
-        char tmp[4][4];
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                tmp[j][3 - i] = shape[i][j];
-        memcpy(shape, tmp, sizeof(shape));
-    }
-};
-
-class SPiece : public Piece {
-public:
-    SPiece() {
-        char tmp[4][4] = {
-            {' ','S','S',' '},
-            {'S','S',' ',' '},
-            {' ',' ',' ',' '},
-            {' ',' ',' ',' '}
-        };
-        memcpy(shape, tmp, sizeof(shape));
-    }
-    void rotate() override {
-        char tmp[4][4];
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                tmp[j][3 - i] = shape[i][j];
-        memcpy(shape, tmp, sizeof(shape));
-    }
-};
-
-class ZPiece : public Piece {
-public:
-    ZPiece() {
-        char tmp[4][4] = {
-            {'Z','Z',' ',' '},
-            {' ','Z','Z',' '},
-            {' ',' ',' ',' '},
-            {' ',' ',' ',' '}
-        };
-        memcpy(shape, tmp, sizeof(shape));
-    }
-    void rotate() override {
-        char tmp[4][4];
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                tmp[j][3 - i] = shape[i][j];
-        memcpy(shape, tmp, sizeof(shape));
-    }
-};
-
 Piece* currentPiece = NULL;
 int x = 5, y = 0;
-
 
 void gotoxy(int x, int y) {
     COORD c;
@@ -168,28 +93,12 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
-void boardDelBlock() {
-    if (currentPiece == NULL) return;
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            if (currentPiece->get(i, j) != ' ')
-                board[y + i][x + j] = ' ';
-}
-
-void block2Board() {
-    if (currentPiece == NULL) return;
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            if (currentPiece->get(i, j) != ' ')
-                board[y + i][x + j] = currentPiece->get(i, j);
-}
-
 void initBoard() {
     for (int i = 0; i < H; i++)
         for (int j = 0; j < W; j++)
-            if ((i == H - 1) || (j == 0) || (j == W - 1)) 
+            if (i == H - 1 || j == 0 || j == W - 1)
                 board[i][j] = '#';
-            else 
+            else
                 board[i][j] = ' ';
 }
 
@@ -197,19 +106,31 @@ void draw() {
     gotoxy(0, 0);
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
-            if (board[i][j] == '#') 
-                cout << (char)178 << (char)178;
-            else if (board[i][j] == ' ') 
-                cout << "  ";
-            else 
-                cout << "[]";
+            if (board[i][j] == '#') cout << (char)178 << (char)178;
+            else if (board[i][j] == ' ') cout << "  ";
+            else cout << "[]";
         }
         cout << endl;
     }
 }
 
+void boardDelBlock() {
+    if (!currentPiece) return;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (currentPiece->get(i, j) != ' ')
+                board[y + i][x + j] = ' ';
+}
+
+void block2Board() {
+    if (!currentPiece) return;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (currentPiece->get(i, j) != ' ')
+                board[y + i][x + j] = currentPiece->get(i, j);
+}
+
 bool canMove(int dx, int dy) {
-    if (currentPiece == NULL) return false;
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             if (currentPiece->get(i, j) != ' ') {
@@ -221,59 +142,73 @@ bool canMove(int dx, int dy) {
     return true;
 }
 
+bool canRotate() {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (currentPiece->get(i, j) != ' ') {
+                int tx = x + j;
+                int ty = y + i;
+                if (tx < 1 || tx >= W - 1 || ty >= H - 1) return false;
+                if (board[ty][tx] != ' ') return false;
+            }
+    return true;
+}
+
 void removeLine() {
-    int j;
-    bool removed = false;
     for (int i = H - 2; i > 0; i--) {
-        for (j = 1; j < W - 1; j++)
-            if (board[i][j] == ' ') break;
-        if (j == W - 1) {
-            removed = true;
+        bool full = true;
+        for (int j = 1; j < W - 1; j++)
+            if (board[i][j] == ' ') { full = false; break; }
+
+        if (full) {
             for (int ii = i; ii > 0; ii--)
-                for (int k = 1; k < W - 1; k++)
-                    board[ii][k] = board[ii - 1][k];
+                for (int j = 1; j < W - 1; j++)
+                    board[ii][j] = board[ii - 1][j];
             i++;
         }
     }
-    if (removed && speed > 30) speed -= 10;
 }
 
 int main() {
     srand(time(0));
     system("cls");
     initBoard();
-    currentPiece = new IPiece(); 
+
+    currentPiece = new TPiece();
 
     while (true) {
         boardDelBlock();
+
         if (kbhit()) {
             char c = getch();
             if (c == 'a' && canMove(-1, 0)) x--;
             if (c == 'd' && canMove(1, 0)) x++;
             if (c == 's' && canMove(0, 1)) y++;
+
             if (c == 'w') {
                 currentPiece->rotate();
-                if (!canMove(0, 0)) {
-                }
+                if (!canRotate())
+                    currentPiece->rotateBack();
             }
+
             if (c == 'q') break;
         }
-        if (canMove(0, 1)) {
+
+        if (canMove(0, 1))
             y++;
-        } else {
+        else {
             block2Board();
             removeLine();
             delete currentPiece;
-            currentPiece = new TPiece(); 
-            
-            x = 5; 
-            y = 0;
-            if (!canMove(0, 0)) break;
+            currentPiece = new TPiece();
+            x = 5; y = 0;
         }
+
         block2Board();
         draw();
         Sleep(speed);
     }
+
     delete currentPiece;
     return 0;
 }
